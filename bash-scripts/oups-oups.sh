849622ece60522126cd52fd979f5c4b2bed37244
sabativi <victor.sabatier@gmail.com>
2019-02-19 13:21:20 +0100

    update script

--- bash-scripts/oups-oups.sh
+++ bash-scripts/oups-oups.sh
@@ -3,7 +3,8 @@
 if [ -n "$(git status --porcelain)" ]; then
   echo "Il y a des changements en cours, commitez les d'abord ou supprimez les";
 else
-  git pull origin amend
+  git remote add fork https://github.com/le-campus-numerique/git-ecommerce.git
+  git pull fork amend-bis
   git reset HEAD^
   git add src/index.html
   git commit -m 'Oh I miss the image'
